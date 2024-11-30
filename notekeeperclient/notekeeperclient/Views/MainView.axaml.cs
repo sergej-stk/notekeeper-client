4c6e83fc41aeaf7227d2d2860a3a6c682b8534cf
Sergej Steinsiek <sergejsteinsiek@proton.me>
2024-11-30 17:02:56 +0100

    inital commit

---
+++ notekeeperclient/notekeeperclient/Views/MainView.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+
+using notekeeperclient.ViewModels;
+
+namespace notekeeperclient.Views;
+
+public partial class MainView : UserControl
+{
+    // This constructor is used when the view is created by the XAML Previewer
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    // This constructor is used when the view is created via dependency injection
+    public MainView(MainViewModel viewModel)
+        : this()
+    {
+        DataContext = viewModel;
+    }
+}
