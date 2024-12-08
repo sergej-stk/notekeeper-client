9dffa10897ef843b1e7b027cc995f4cc0029e093
Sergej Steinsiek <sergejsteinsiek@proton.me>
2024-12-08 15:54:27 +0100

    Remove unused code

--- notekeeperclient/notekeeperclient/ViewModels/MainViewModel.cs
+++ notekeeperclient/notekeeperclient/ViewModels/MainViewModel.cs
@@ -5,11 +5,5 @@ namespace notekeeperclient.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
-    public string Greeting => "Welcome to Avalonia!";
 
-    [ObservableProperty]
-    private int _counter;
-
-    [RelayCommand]
-    public void Increment() => Counter++;
 }
